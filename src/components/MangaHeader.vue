027c57a2de297059dbee6e50d006328322459756
hwen <hwenleung@gmail.com>
2018-01-30 12:10:45 +0800

    update

--- src/components/MangaHeader.vue
+++ src/components/MangaHeader.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-.manga-header(:style='styleObj')
+.manga-header
+  .manga-title 陆地沉没记 ~ 少年S的记录 ~
+  .manga-layer
+  .manga-background(:style='styleObj')
+  .manga-triangle
 </template>
 <script>
 export default {
@@ -26,11 +30,49 @@ export default {
 <style lang="scss">
 .manga-header {
   height: 200px;
-  width: 100%;
-  background: url('./../assets/Senjougahara-huaji.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  opacity: 0.5;
+  position: relative;
+  .manga-background {
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    background: url('./../assets/Senjougahara-huaji.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    position: absolute;
+    z-index: -1;
+    opacity: 0.2;
+  }
+  .manga-layer {
+    position: absolute;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    opacity: 0.2;
+    background: #8b80f9;
+  }
+  .manga-triangle {
+    z-index: 2;
+    position: absolute;
+    width: 0;
+    height: 50;
+    left: 0;
+    top: 150px;
+    border-style: solid;
+    border-width: 0 100vw 50px 0 ;
+    border-color: white transparent;
+  }
+  .manga-title {
+    position: absolute;
+    top: 45%;
+    left: 0;
+    right: 0;
+    text-align: center;
+    font-size: 24px;
+    color: #606c76;
+  }
 }
 </style>
--- src/pages/LandSunk.vue
+++ src/pages/LandSunk.vue
@@ -16,6 +16,9 @@ export default {
       items
     }
   },
+  created () {
+    logs('land sunk part')
+  },
   methods: {
 
   }
